6b5a20741134e1982815a2cbedbc268428835c8b
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-10 10:57:26 +0500

    add index and js files

---
+++ app.js
@@ -0,0 +1,26 @@
+
+//Q=1
+
+// window.alert("Error! plesae enter a valid password.")
+ 
+
+
+//Q=2
+//window.alert("Welcome to JS Land...\nHappy Coding!");
+//Q=3
+
+//window.alert("Welcome to JS Land...")
+
+//Q=4
+// alert ("Prevent this page from creating additional dialogs.")z
+
+
+
+
+
+//Q=4
+//window.alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+//Q=5
+
+//window.alert("Hello...I can run JS through my web browser's console")
